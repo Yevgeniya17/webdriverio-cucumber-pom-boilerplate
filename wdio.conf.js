c11aa39550c88a5cf05e1c6c777ad057d51cb196
QASchoolUSA <qaschoolusa@gmail.com>
2020-08-13 23:25:08 -0400

    Changed the reporter to Spec reporter.

--- wdio.conf.js
+++ wdio.conf.js
@@ -155,7 +155,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['dot'],
+    reporters: ['dot', 'spec'],
 
     //
     // If you are using Cucumber you need to specify the location of your step definitions.
